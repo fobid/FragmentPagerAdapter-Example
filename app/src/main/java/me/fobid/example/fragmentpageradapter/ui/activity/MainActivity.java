e8060ebe78cfd4896b464d79dfdce7bd6b8626a6
Fobid <fobidbumz@naver.com>
2016-09-28 20:15:49 +0900

    Add project

---
+++ app/src/main/java/me/fobid/example/fragmentpageradapter/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package me.fobid.example.fragmentpageradapter.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.fobid.example.fragmentpageradapter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_main);
+    }
+}
